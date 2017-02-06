48f04cb94de37a99e800e7747a17f5494d4ac2b0
Tom Barton <trb1g14@soton.ac.uk>
2017-02-06 18:22:31 +0000

    Initial Commit

---
+++ www/app.js
@@ -0,0 +1,17 @@
+/**
+ * Created by tombarton on 06/02/2017.
+ */
+$(document).ready(function () {
+
+  $('#jsSsNotesButton').click(function () {
+    location.href = "https://secure.ecs.soton.ac.uk/noteswiki/w/COMP3217";
+  });
+
+  $('#jsDbNotesButton').click(function () {
+    location.href = "https://secure.ecs.soton.ac.uk/noteswiki/w/COMP3211";
+  });
+
+  $('#jsP3pNotesButton').click(function () {
+    location.href = "https://secure.ecs.soton.ac.uk/module/1617/COMP3200/33192/";
+  });
+});
